f43a1d5064153b579bce85541e23171a3ce1ad28
whoisyuri <buryak@gmail.com>
2014-03-04 14:07:25 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Junkflag::Application.config.secret_key_base = '45045cd3141981fccb800ed95614d5f5499d939d1c65dae63f67e8fef18bcdcd7e31ddd57085388aa1e3af346aea23fc4a8fee926ca038b947c99c650edb6667'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Junkflag::Application.config.session_store :cookie_store, key: '_junkflag_session'
